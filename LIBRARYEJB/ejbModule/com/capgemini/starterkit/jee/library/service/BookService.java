4727a9f199e1bcf6e7f00a4f4e866702bd83be2c
KSedlak <krzysztof.sedlak@hotmail.com>
2015-10-14 15:45:03 +0200

    add service and make jpa

---
+++ LIBRARYEJB/ejbModule/com/capgemini/starterkit/jee/library/service/BookService.java
@@ -0,0 +1,20 @@
+package com.capgemini.starterkit.jee.library.service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class BookService
+ */
+@Stateless
+@LocalBean
+public class BookService {
+
+    /**
+     * Default constructor. 
+     */
+    public BookService() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
