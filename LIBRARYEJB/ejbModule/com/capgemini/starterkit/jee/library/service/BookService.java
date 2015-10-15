703e08c18cd8c2ba48f685e49850178b60f2e209
KSedlak <krzysztof.sedlak@hotmail.com>
2015-10-15 12:07:49 +0200

    extend BookService

--- LIBRARYEJB/ejbModule/com/capgemini/starterkit/jee/library/service/BookService.java
+++ LIBRARYEJB/ejbModule/com/capgemini/starterkit/jee/library/service/BookService.java
@@ -1,13 +1,12 @@
 package com.capgemini.starterkit.jee.library.service;
 
 import java.util.List;
-
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import com.capgemini.starterkit.jee.library.entities.BOOK;
+
 
 /**
  * Session Bean implementation class BookService
@@ -16,16 +15,43 @@ import com.capgemini.starterkit.jee.library.entities.BOOK;
 @LocalBean
 public class BookService {
 
-    public BookService() {
+	public BookService() {
 
-    }
-    
-    @PersistenceContext(unitName = "LIBRARY_PU")
+	}
+
+	@PersistenceContext(unitName = "LIBRARY_PU")
 	EntityManager em;
 
 	public List<BOOK> findBOOKs() {
 		return em.createQuery("Select book From BOOK book", BOOK.class)
 				.getResultList();
+	}
+
+	public BOOK addBOOK(BOOK b) {
+		em.persist(b);
+		return b;
+	}
+
+	public BOOK removeBOOK(BOOK b) {
+		em.remove(b);
+		return b;
+	}
+
+	public BOOK editBOOK(BOOK b) {
+		BOOK fromDB = em.find(BOOK.class, b.getId());
+		fromDB.setTitle(b.getTitle());
+		em.persist(fromDB);
+		return fromDB;
+
+	}
+
+	public List<BOOK> findByTitle(String title) {
+		@SuppressWarnings("unchecked")
+		List<BOOK> resultList = em
+				.createQuery(
+						"select book from BOOK book where upper(book.title) like concat(upper(:title), '%')")
+				.setParameter("title", title).getResultList();
+		return resultList;
 	}
 
 }
